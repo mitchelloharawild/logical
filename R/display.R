ef96ebbac821ceece23afca2c5017eb2f9354402
Mitchell <mitch.ohara-wild@monash.edu>
2019-03-23 14:03:39 +1100

    Added knit_print for latex output

--- R/display.R
+++ R/display.R
@@ -10,9 +10,24 @@ tbl_sum.truth_df <- function(x){
 #' @export
 knit_print.truth_df <- function(x, ...){
   if(knitr::is_latex_output()){
-    NextMethod()
+    colnames(x) <- paste0("$", convert_symbolic(colnames(x), output = "tex"), "$")
+    knitr::knit_print(knitr::kable(x))
   }
   else{
     abort("Displaying truth tables outside of LaTeX is not yet supported.")
   }
+}
+
+symbols <- list(
+  list(code = "%=>%", tex = "{\\Rightarrow}", unicode = "\U21D2"),
+  list(code = "%iff%", tex = "{\\Leftrightarrow}", unicode = "\U21D4"),
+  list(code = "!", tex = "{\\lnot}", unicode = "\U00AC")
+)
+
+#' @export
+convert_symbolic <- function(x, output = c("code", "tex", "unicode")){
+  output <- match.arg(output)
+  reduce(symbols, function(x, replacement){
+    gsub(replacement$code, replacement[[output]], x, fixed = TRUE)
+  }, .init = x)
 }
---
+++ R/utils.R
@@ -0,0 +1,4 @@
+reduce <- function(.x, .f, ..., .init) {
+  f <- function(x, y) .f(x, y, ...)
+  Reduce(f, .x, init = .init)
+}
